2210531894d415284cc7fb1a3e27a814cc3abca6
cindaantonia948-art <cindaantonia948@gmail.com>
2026-05-31 15:39:44 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = "despha-v1";
+const FILES = [
+  "/santo/index.html",
+  "/santo/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
